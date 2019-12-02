29179164e654376db7c84d9ed249a8aab5ffc5c5
o_o <smithtrombone@gmail.com>
2019-12-02 12:21:58 -0500

    initialized git repository

---
+++ cronch.cpp
@@ -0,0 +1,12 @@
+#include "cronch.h"
+
+int main (int argc, char **argv)
+{
+	if (argc < 2)
+		return 1;
+
+	if (argc < 3)
+		return 2;
+
+	return;
+}
---
+++ cronch.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+
+fstream cronchFile;
+
+int compress (char *file);
+int decompress (char *file);
