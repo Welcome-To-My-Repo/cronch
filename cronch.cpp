82dbc1b7f92db6d7f932d3fc931e514874bb671e
o_o <smithtrombone@gmail.com>
2019-12-03 08:20:37 -0500

    ...

--- cronch.cpp
+++ cronch.cpp
@@ -22,17 +22,56 @@ int compress ()
 {
 	std::ifstream cronchIN;
 	std::ofstream cronchOUT;
+	char _32bit [4];
+	int8_t _8bit;
+	int64_t collate_to_decimal = 0;
 	cronchOUT.open ("file.cronch", std::ios::out | std::ios::binary);
 	cronchIN.open ("test/test.txt", std::ios::in | std::ios::binary);
-	char bit64 [4];
-	int8_t bit8;
 	if (!cronchIN.is_open ())
 		return 0;
-	while (!cronchIN.eof ())
-	{
-		cronchIN.read (bit64, 4);
-		cronchOUT.write (bit64, 4);
-	}
+	//while (!cronchIN.eof ())
+	//{
+		cronchIN.read (_32bit, 4);
+		//cronchOUT.write (bit64, 4);
+		_8bit = _32bit[0];
+		for (int j = 7; _8bit > 0; j--)
+		{
+			binarycollate[j] = _8bit%2;
+			_8bit = _8bit/2;
+		}
+		_8bit = _32bit[1];
+		for (int j = 15; _8bit > 0; j--)
+		{
+			binarycollate[j] = _8bit%2;
+			_8bit = _8bit/2;
+		}
+		_8bit = _32bit[2];
+		for (int j = 23; _8bit > 0; j--)
+		{
+			binarycollate[j] = _8bit%2;
+			_8bit = _8bit/2;
+		}
+		_8bit = _32bit[3];
+		for (int j = 31; _8bit > 0; j--)
+		{
+			binarycollate[j] = _8bit%2;
+			_8bit  = _8bit/2;
+		}
+		for (int i = 0; i < 32; i ++)
+			std::cout << binarycollate[i] << " ";
+		std::cout << std::endl;
+		int64_t max = _4byte;
+		for (int i = 0; i < 32; i++)
+		{
+			std::cout << "maximum: " << max << std::endl;
+			if (binarycollate[i])
+			{
+				collate_to_decimal = collate_to_decimal + max;
+			}
+			max = max/2;
+			std::cout << "collated total: " << collate_to_decimal << std::endl;
+		}
+	//}
 	return 0;
 
 }
--- cronch.h
+++ cronch.h
@@ -4,8 +4,41 @@
 #include <string>
 #include <cstring>
 #include <cstdint>
+#include <vector>
 
 std::fstream cronchFile;
 
 int compress ();
 int decompress ();
+
+static const int
+	P_1 = 1,
+	P_2 = 2,
+	P_3 = 3,
+	P_4 = 5,
+	P_5 = 7,
+	P_6 = 11,
+	P_7 = 13,
+	P_8 = 17,
+	P_9 = 19,
+	P_10 = 23,
+	P_11 = 29,
+	P_12 = 31,
+	P_13 = 37,
+	P_14 = 41,
+	P_15 = 43;
+
+struct EightBit
+{
+	bool
+		_128,
+		_64,
+		_32,
+		_16,
+		_8,
+		_4,
+		_2,
+		_1;
+};
+bool binarycollate [32];
+static int64_t _4byte = 4294967295;
