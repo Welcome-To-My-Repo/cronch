0393d3a872e302c03dcbeee9fa76c16e1421d756
o_o <smithtrombone@gmail.com>
2019-12-17 16:11:19 -0500

    ...

--- cronch.cpp
+++ cronch.cpp
@@ -3,7 +3,7 @@
 int main (int argc, char **argv)
 {
 //initialize variables
-		std::string all64bits, leftovers, temp;
+		std::string all32bits, leftovers, temp;
 		std::bitset <32>* all8char;
 		std::bitset <8> charset[4];
 		char readin [4], get[1];
@@ -68,14 +68,14 @@ int main (int argc, char **argv)
 	{
 //check if filesize can be completely sectioned into 8 byte blocks
 //if not, adjust number of blocks to be compressed
-			if (filesize%8 == 0)
+			if (filesize%4 == 0)
 			{
-				filesize = filesize/8;
+				filesize = filesize/4;
 				is_leftovers = false;
 			}
 			else
 			{
-				filesize = 1 + filesize/8;
+				filesize = 1 + filesize/4;
 				is_leftovers = true;
 			}
 			std::cout << "adjusted repetitions: " << filesize << std::endl;
@@ -83,18 +83,19 @@ int main (int argc, char **argv)
 		for (int i = 0; i < filesize; i ++)
 		{
 			std::cout << "loop start" << std::endl;
-			all64bits.clear ();
+			all32bits.clear ();
 			factors.clear ();
 			temp.clear ();
 			is_factoring = true;
 			factors.clear ();
 			//run_comp_factors.clear ();
 //set the input string to default
-			for (int a = 0; a < 8; a++)
+			for (int a = 0; a < 4; a++)
 				readin[a] = -1;
 //check if 8 bytes can still be read from the file;
 			if (i == filesize - 1 and is_leftovers)
 			{
+				std::cout << "apppending remaining bytes..." << std::endl;
 				while (cronchIN.get (get, 1))
 					cronchOUT.write (get, 1);
 				break;
@@ -102,17 +103,17 @@ int main (int argc, char **argv)
 			else
 			{
 //read in 8 bytes
-				cronchIN.read (readin, 8);
+				cronchIN.read (readin, 4);
 			}
-			std::cout << "8 byte block: " << readin << std::endl;
+			std::cout << "4 byte block: " << readin << std::endl;
 //separate the 8 bytes
-				for (int i = 0; i < 8; i ++)
+				for (int i = 0; i < 4; i ++)
 					charset [i] = std::bitset <8> (readin[i]);
 //take the binary representation of each byte and combine them into a 64 bit binary value
-				for (int i = 0; i < 8; i++)
-					all64bits.append (charset[i].to_string ());
+				for (int i = 0; i < 4; i++)
+					all32bits.append (charset[i].to_string ());
 //put the 64 bit binary value into a bitset class because...
-				all8char = new std::bitset <64> (all64bits.c_str ());
+				all8char = new std::bitset <32> (all32bits.c_str ());
 //convert the binary representation to a 64 bit integer
 				original = all8char->to_ullong ();
 //duplicate to preserve the initial value
@@ -121,11 +122,17 @@ int main (int argc, char **argv)
 				remainder = dividend%primes[14];
 				prime = 14;
 				factorProduct = 1;
+				factoring = true;
+				std::cout << "list of factors: " << std::endl;
 				while (factoring)
 				{
 					for (int a = 14; a > -1; a --)
 						if (primes[a] == dividend)
+						{
+							factors.push_back (a);
+							factoring = false;
 							break;
+						}
 					for (int a = 14; a > -1; a --)
 					{
 						if (dividend%primes[a] < remainder)
@@ -133,16 +140,23 @@ int main (int argc, char **argv)
 							prime = a;
 						}
 					}
+					std::cout << primes[prime] << ", ";
 					dividend = dividend/primes[prime];
 					factors.push_back (prime);
-					factorProduct = factorProduct * primes[prime];
+					factorProduct = factorProduct * primes[prime] + dividend%primes[prime];
 				}
+				std::cout << std::endl;
 				difference = original - factorProduct;
+				std::cout << "original: " << original << std::endl
+				<< "dividend: " << dividend << std::endl
+				<< "factorProduct: " << factorProduct << std::endl
+				<< "diference: " << difference << std::endl << std::endl;;
 //combine same factors
 				for (int j = 0; j < factors.size (); j ++)
 				{
 					run_comp_factors.at (factors.at (j)) ++;
 				}
+				std::cout << "writing out factors:" << std::endl;
 				for (int j = 0; j < 15; j ++)
 				{
 					if (run_comp_factors.at (j) != 0)
@@ -151,31 +165,39 @@ int main (int argc, char **argv)
 						{
 							for (int a = 0; a < run_comp_factors.at (j)/15; a ++)
 							{
+								std::cout << "prime is a factor > 15 times..." << std::endl;
 								temp.clear ();
 								write_factors[0] = std::bitset<4> (15);
 								write_factors[1] = std::bitset<4> (j);
-								temp.push_back (write_factors[0].to_string ());
-								temp.push_back (write_factors[1].to_string ());
+								temp.append (write_factors[0].to_string ());
+								temp.append (write_factors[1].to_string ());
+								std::cout << "byte: " << temp << std::endl;
 								writeout = new std::bitset<8> (temp.c_str ());
 								cronchPUT[0] = (char)writeout->to_ulong ();
 								cronchOUT.write (cronchPUT, 1);
 							}
-							temp.clear ();
-							write_factors[0] = std::bitset<4> (run_comp_factors.at(j)-(15 * (run_comp_factors.at(j)/15)));
-							write_factors[1] = std::bitset<4> (j);
-							temp.push_back (write_factors[0].to_string ());
-							temp.push_back (write_factors[1].to_string ());
-							writeout = new std::bitset<8> (temp.c_str ());
-							cronchPUT[0] = (char)writeout->to_ulong ();
-							cronchOUT.write (cronchPUT, 1);
+							if (run_comp_factors.at (j)%15 > 0)
+							{
+								std::cout << "writing out remaining times prime is a factor" << std::endl;
+								temp.clear ();
+								write_factors[0] = std::bitset<4> (run_comp_factors.at(j)-(15 * (run_comp_factors.at(j)/15)));
+								write_factors[1] = std::bitset<4> (j);
+								temp.append (write_factors[0].to_string ());
+								temp.append (write_factors[1].to_string ());
+								std::cout << "byte: " << temp << std::endl;
+								writeout = new std::bitset<8> (temp.c_str ());
+								cronchPUT[0] = (char)writeout->to_ulong ();
+								cronchOUT.write (cronchPUT, 1);
+							}
 						}
 						else
 						{
 							temp.clear ();
 							write_factors[0] = std::bitset <4> (run_comp_factors.at (j));
 							write_factors[1] = std::bitset <4> (j);
-							temp.push_back (write_factors[0].to_string ());
-							temp.push_back (write_factors[1].to_string ());
+							temp.append (write_factors[0].to_string ());
+							temp.append (write_factors[1].to_string ());
+							std::cout << "byte: " << temp << std::endl;
 							writeout = new std::bitset<8> (temp.c_str ());
 							cronchPUT[0] = (char)writeout->to_ulong ();
 							cronchOUT.write (cronchPUT, 1);
@@ -183,72 +205,73 @@ int main (int argc, char **argv)
 					}
 				}
 //append the leftover really big prime number
-				if (dividend > 0)
+				if (difference > 0)
 				{
-					if (dividend > UINT8_MAX)
+					std::cout << "writing difference..." << std::endl;
+					if (difference > UINT8_MAX)
 					{
-						if (dividend > UINT16_MAX)
+						if (difference > UINT16_MAX)
 						{
+							std::cout << "writing 4 byte value" << std::endl;
 							cronchPUT[0] = 3;
 							cronchOUT.write (cronchPUT, 1);
-							long_out_32 = new std::bitset<32> (dividend);
+							long_out_32 = new std::bitset<32> (difference);
+							std::cout << *long_out_32 << std::endl;
 							short_out = new std::bitset <8> ();
 							for (int i = 24; i < 32; i++)
 							{
 								(*short_out)[i] = (*long_out_32)[i];
 							}
-							to_char_convert = short_out->to_ullong ();
-							cronchPUT[0] = (char)to_char_convert;
+							cronchPUT[0] = (char)short_out->to_ullong ();
 							cronchOUT.write (cronchPUT, 1);
 							for (int i = 16; i < 24; i++)
 							{
 								(*short_out)[i] = (*long_out_32)[i];
 							}
-							to_char_convert = short_out->to_ullong ();
-							cronchPUT[0] = (char)to_char_convert;
+							cronchPUT[0] = (char)short_out->to_ullong ();
 							cronchOUT.write (cronchPUT, 1);
 							for (int i = 8; i < 16; i++)
 							{
 								(*short_out)[i] = (*long_out_32)[i];
 							}
-							to_char_convert = short_out->to_ullong ();
-							cronchPUT[0] = (char)to_char_convert;
+							cronchPUT[0] = (char)short_out->to_ullong ();
 							cronchOUT.write (cronchPUT, 1);
 							for (int i = 0; i < 8; i++)
 							{
 								(*short_out)[i] = (*long_out_32)[i];
 							}
-							to_char_convert = short_out->to_ullong ();
-							cronchPUT[0] = (char)to_char_convert;
+							cronchPUT[0] = (char)short_out->to_ullong ();
 							cronchOUT.write (cronchPUT, 1);
 						}
 						else
 						{
+							std::cout << "writing 2 byte value" << std::endl;
 							cronchPUT[0] = 2;
 							cronchOUT.write (cronchPUT, 1);
-							long_out_16 = new std::bitset <16> (dividend);
+							long_out_16 = new std::bitset <16> (difference);
+							std::cout << *long_out_16 << std::endl;
 							short_out = new std::bitset <8> ();
 							for (int i = 8; i < 16; i++)
 							{
 								(*short_out)[i] = (*long_out_16)[i];
 							}
-							to_char_convert = short_out->to_ullong ();
-							cronchPUT[0] = (char)to_char_convert;
+							cronchPUT[0] = (char)short_out->to_ullong ();
 							cronchOUT.write (cronchPUT, 1);
 							for (int i = 0; i < 8; i++)
 							{
 								(*short_out)[i] = (*long_out_16)[i];
 							}
-							to_char_convert = short_out->to_ullong ();
-							cronchPUT[0] = (char)to_char_convert;
+							cronchPUT[0] = (char)short_out->to_ullong ();
 							cronchOUT.write (cronchPUT, 1);
 						}
 					}
 					else
 					{
+						std::cout << "writing 1 byte value" << std::endl;
 						cronchPUT[0] = 1;
 						cronchOUT.write (cronchPUT, 1);
-						cronchPUT[0] = (char)dividend;
+						cronchPUT[0] = (char)difference;
+						std::cout << difference << std::endl;
 						cronchOUT.write (cronchPUT, 1);
 					}
 				}
